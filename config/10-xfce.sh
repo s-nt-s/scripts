38598bff7ab809c57700b0601b65080605206792
s-nt-s <santos82h@gmail.com>
2017-02-20 20:55:52 +0100

    batería

--- config/10-xfce.sh
+++ config/10-xfce.sh
@@ -20,7 +20,7 @@ xfconf-query -c xfwm4 -p /general/workspace_count -s 2
 xfconf-query -c xfwm4 -p /general/workspace_names -s "[1]" -s "[2]" -s "[3]" -s "[4]"
 xfconf-query -c xfce4-panel -p /panels/panel-1/background-style -s 0
 xfconf-query -c xfce4-panel -p /panels/panel-1/size -s 25
-xfconf-query -c xfce4-session -p /compat/LaunchGNOME -s true
+xfconf-query -c xfce4-session -np /compat/LaunchGNOME -t 'bool' -s 'true'
 xfconf-query -c xfwm4 -p /general/use_compositing -s false
 xfconf-query -c xfwm4 -p /general/wrap_windows -s false
 xfconf-query -c xfwm4 -p /general/wrap_resistance -s 5
@@ -30,4 +30,22 @@ xfconf-query -c xfwm4 -p /general/box_move -s true
 xfconf-query -c xfce4-notifyd -p /initial-opacity -s 1
 xfconf-query -c xfce4-notifyd -p /notify-location -s 3
 xfconf-query -c xsettings -p /Net/IconThemeName -s gnome-brave
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/show-tray-icon -s 1
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/inactivity-sleep-mode-on-battery -s 1
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/inactivity-sleep-mode-on-ac -s 1
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/inactivity-on-battery -s 14
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/inactivity-on-ac -s 14
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/critical-power-action -s 3
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/dpms-enabled -s false
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/brightness-on-battery -s 9
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/brightness-on-ac -s 9
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/blank-on-battery -s 0
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/blank-on-ac -s 0
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/power-button-action -s 4
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/sleep-button-action -s 1
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/hibernate-button-action -s 0
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/lid-action-on-battery -s 1
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/lid-action-on-ac -s 0
+xfconf-query -c xfce4-power-manager -p /xfce4-power-manager/logind-handle-lid-switch -s true
+
 
