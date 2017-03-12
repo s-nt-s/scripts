439171ad0a7ca170ba813ad0e474c2616c6afeb9
s-nt-s <santos82h@gmail.com>
2017-03-12 19:54:31 +0100

    typo

--- _enlazar.sh
+++ _enlazar.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-./enlaza.sh $(find ../ -name "uso.pl" -o -name "mkgit.sh" -o -name "meteo.pl" -o -name "up.pl" -o -name "say.sh" -o -name "miepub.sh") enlaza.sh brillo.sh getip.sh pocket.sh
+./enlaza.sh $(find ../ -name "uso.pl" -o -name "mkgit.sh" -o -name "meteo.pl" -o -name "up.pl" -o -name "say.sh" -o -name "miepub.sh") enlaza.sh getip.sh pocket.sh
 
