e2a8db97b0320d9b9aebf5938cabab7028eaf7b0
s-nt-s <santos82h@gmail.com>
2018-05-15 11:51:36 +0200

    update hosts

--- update-hosts.sh
+++ update-hosts.sh
@@ -1,32 +1,72 @@
 #!/bin/bash
 
+TARGET="/etc/hosts"
+
 if [ "$1" = "--install" ]; then
     FILE=$(basename $0)
     REAL=$(realpath $0)
     CMD=${FILE%.*}
-    LINK="/etc/network/if-up.d/$CMD"
-    ln -f -s "$REAL" "$LINK"
-    echo "Now you can create hosts files for your WIFIs like that:"
-    for wiki in $(nmcli -t -f active,ssid dev wifi | cut -d: -f2 | head -n 3); do
-        echo "  /etc/hosts.wifi.$wiki"
-    done
+    LINK1="/etc/network/if-up.d/$CMD"
+    LINK2="/etc/network/if-post-down.d/$CMD"
+    ln -f -s "$REAL" "$LINK1"
+    ln -f -s "$REAL" "$LINK2"
+    echo "Now you can do custom zones in $TARGET like that:"
+    echo '
+#### HOME
+##
+## SSID WIFIA64E
+##  MAC c4:05:20:32:b8:4b
+##
+#
+# 192.168.1.30    external.domain.com
+#
+####
+
+so
+# 192.168.1.30    external.domain.com
+will be uncommented when you are connected to WIFIA64E by WIFI or a router with mac c4:05:20:32:b8:4b'
     exit 0
 fi
 
+#SSID=$(LANG=en_EN nmcli -t -f active,ssid dev wifi | egrep '^yes' | cut -d: -f2)
+SSID=$(iwgetid -r)
+MAC=""
 
-WIFI=$(LANG=en_EN nmcli -t -f active,ssid dev wifi | egrep '^yes' | cut -d: -f2)
-TARGET="/etc/hosts"
-
-for hosts_file in /etc/hosts.wifi.*; do
-	while read line; do
-		if [ ! -z "$line" ]; then
-			sed "/\s*$line\s*/d" -i "$TARGET"
-		fi
-	done < $hosts_file
-done
-
-hosts_file="/etc/hosts.wifi.$WIFI"
-if [ -f "$hosts_file" ]; then
-    cat "$hosts_file" >> $TARGET
+ROUTER=$(ip route show match 0/0 | grep default | awk '{print $3}' | sort | uniq)
+if [ ! -z "$ROUTER" ]; then
+    MAC=$(ip neigh | grep $ROUTER | awk '{print $5}' | sort | uniq)
 fi
+
+awk -i inplace -v SSID="$SSID" -v MAC="$MAC" '
+    BEGIN {
+        config_zone=0;
+        comment=1;
+    }
+    $1 == "####" {
+        comment=1
+        if (config_zone==0) config_zone = 1;
+        else config_zone = 0;
+        print $0;
+        next;
+    }
+    config_zone == 1 && $1 == "##" && ($2 == "SSID" || $2 == "MAC") {
+        if ($2 == "SSID" && SSID != "" && SSID == $3) comment = 0;
+        if ($2 == "MAC"  &&  MAC != "" &&  MAC == $3) comment = 0;
+        print $0;
+        next;
+    }
+    config_zone == 1 && $1 == "#" && comment == 0 {
+        line = $0
+        sub(/^#\s*/,"", line);
+        print line;
+        next;
+    }
+    config_zone == 1 && $1 !~ /^#+/ && comment == 1 {
+        print "#", $0;
+        next;
+    }
+    {
+        print $0;
+    }
+' $TARGET
 
