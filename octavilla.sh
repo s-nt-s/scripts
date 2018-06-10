882b764c03ab424fb4183ffbfbf6652814feba95
s-nt-s <santos82h@gmail.com>
2018-06-10 21:13:59 +0200

    parametrizar

--- octavilla.sh
+++ octavilla.sh
@@ -10,6 +10,8 @@ if [[ ! -f "$1" ]]; then
     exit 0
 fi
 
+ITER=9
+GRID=""
 SOURCE=$(realpath -- "$1")
 
 DIR="$PWD"
@@ -43,7 +45,12 @@ if [ $pages -ne 2 ]; then
 fi
 
 function build_page {
-    pdftk "$WORKCOPY" cat $1 $1 $1 $1 $1 $1 $1 $1 $1 output "$1.pdf"
+    PAG=""
+    for (( c=1; c<=$ITER; c++ ))
+    do
+        PAG="$PAG $1"
+    done
+    pdftk "$WORKCOPY" cat $PAG output "$1.pdf"
     rot=$(pdfinfo "$1.pdf" | grep "Page rot" | awk '{print $3}')
     if [ $rot -ne 0 ]; then
         pdftk "$1.pdf" cat 1-endnorth output "$1_north.pdf"
@@ -54,14 +61,25 @@ function build_page {
     width=$(echo $size | cut -d' ' -f1)
     height=$(echo $size | cut -d' ' -f2)
     pdf2ps "$1.pdf" "$1.ps"
-    psnup -W${width} -H${height} -pa4 -9 "$1.ps" "$1_oct.ps"
+    psnup -W${width} -H${height} -pa4 -${ITER} "$1.ps" "$1_oct.ps"
 
     if [ $1 -eq 1 ]; then
         # Guias para cortar
         # Ancho: 842
         #  Alto: 595
         cp "$1_oct.ps" "$1_oct.bak.ps"
-        cat > "$1_oct.ps" <<EOF
+        get_grid $ITER
+        echo $GRID > "$1_oct.ps"
+        cat "$1_oct.bak.ps" >> "$1_oct.ps"
+    fi
+    rm "$1.pdf"
+    ps2pdf "$1_oct.ps" "$1.pdf"
+    
+}
+
+function get_grid {
+    if [ $1 -eq 9 ]; then
+        read -r -d '' GRID <<EOF
 << /BeginPage
 {
     0.7 setgray
@@ -74,12 +92,8 @@ function build_page {
 }
 >> setpagedevice
 EOF
-        cat "$1_oct.bak.ps" >> "$1_oct.ps"
     fi
-    rm "$1.pdf"
-    ps2pdf "$1_oct.ps" "$1.pdf"
-    
-}  
+}
 
 build_page 1
 build_page 2
