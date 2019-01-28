1a6cdeb6b12e1b6f0eb8a7c65dd7f904ba1b4f16
s-nt-s <santos82h@gmail.com>
2019-01-28 13:49:50 +0100

    excluir juegos que no gustan

--- config/13-pi-games.py
+++ config/13-pi-games.py
@@ -8,7 +8,7 @@ import re
 
 install = len(sys.argv)>1 and sys.argv[1]=="--install"
 
-re_ban = re.compile(r"\b(not-working\.png|speed-slow\.png|emulator|game server)\b", re.IGNORECASE)
+re_ban = re.compile(r"\b(not-working\.png|speed-slow\.png|emulator|game server|desktop companion|GPE)\b", re.IGNORECASE)
 
 r = requests.get("http://www.raspberryconnect.com/raspbian-packages-list/item/65-raspbian-games")
 soup = bs4.BeautifulSoup(r.content, "lxml")
@@ -20,7 +20,9 @@ def get_info(li):
 
 all_games = [(h3.get_text().strip(), h3.find_parent("li"))  for h3 in soup.select("li h3")]
 
-ban_game = set(("python-soya", "balazar", "balazarbrothers", "xabacus", "xmabacus", "xmpuzzles", "xpuzzles"))
+ban_game = set("python-soya balazar balazarbrothers xabacus xmabacus xmpuzzles xpuzzles".split())
+dont_like = set("amor 3dchess filler uligo gpe-tetris gpe-othello gpe-lights gpe-go gsoko orbital-eunuchs-snipe robotfindskitten yics emacs-chess holdingnuts bastet bb bastet dizzy xscreensaver-screensaver-dizzy bambam ktuberling".split())
+ban_game = ban_game.union(dont_like)
 single_game = set()
 dash_game = set()
 for g, li in all_games:
@@ -40,8 +42,8 @@ for g in dash_game:
         continue
     if p in single_game:
         ban_game.add(g)
+        continue
 
-#sys.exit()
 dash_game = dash_game - ban_game - single_game
 single_game = single_game - ban_game
 
