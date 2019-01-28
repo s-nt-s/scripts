ec4b8c8efde8a97afc439f6bd827ee89ad4f680e
s-nt-s <santos82h@gmail.com>
2019-01-28 14:11:19 +0100

    lemario español simplificado

--- otros/wordlist-ascii-es.sh
+++ otros/wordlist-ascii-es.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -Ls http://www.teoruiz.com/lemario/lemario-20101017.txt.gz | zcat | LANG=en_EN sed -e '/^[a-zA-Z]*$/!d' | perl -nle 'print if m{^[[:ascii:]]+$}' | sed -e '/....*/!d'
+curl -Ls http://www.teoruiz.com/lemario/lemario-20101017.txt.gz | zcat | sed  -e 'y/áéíóúü/aeiouu/' -e '/^[a-zA-Z]*$/!d' | perl -nle 'print if m{^[[:ascii:]]+$}' | sed -e '/....*/!d' | sort | uniq
