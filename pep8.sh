ebcb076581254b3aeffeccc72e222ec0d6bf26a1
s-nt-s <santos82h@gmail.com>
2019-05-04 12:34:06 +0200

    añadir shebang si falta

--- pep8.sh
+++ pep8.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 find . -type f -name "*.py" -print0 | xargs -0 sed -i '1 s/^#!\/usr\/bin\/python/#!\/usr\/bin\/env python/'
+find . -type f -executable -name "*.py" | while read f; do
+  fl=$(head -n 1 "$f" | cut -c1-2)
+  if [ "$fl" != "#!" ]; then
+    sed '1i#!/usr/bin/env python3\n' -i "$f"
+  fi
+done
 autoflake --in-place --recursive .
 autopep8 --in-place --recursive .
 isort -rc .
