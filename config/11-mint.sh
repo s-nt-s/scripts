d224e1005d90921308ab765a252f0409b00d00b5
s-nt-s <santos82h@gmail.com>
2019-05-04 11:49:25 +0200

    nuevo paquete

--- config/11-mint.sh
+++ config/11-mint.sh
@@ -12,5 +12,5 @@ sudo apt-get -y install manpages-es manpages-es-extra
 sudo dpkg-reconfigure locales
 
 # aplicaciones
-sudo apt-get -y install virtualbox-nonfree chromium-browser filezilla keepassx calibre transgui chromium-browser-l10n mysql-workbench claws-mail claws-mail-extra-plugins claws-mail-i18n claws-mail-themes geany mkvtoolnix-gui gparted mumble pandoc system-config-samba claws-mail-pgpmime claws-mail-pgpcore
+sudo apt-get -y install virtualbox-nonfree chromium-browser filezilla keepassx calibre transgui chromium-browser-l10n mysql-workbench claws-mail claws-mail-extra-plugins claws-mail-i18n claws-mail-themes geany mkvtoolnix-gui gparted mumble pandoc system-config-samba claws-mail-pgpmime claws-mail-pgpcore hexchat-python3
 
