4836257e7c30e606ea9aa7cfc217cd623891de02
s-nt-s <santos82h@gmail.com>
2019-05-04 11:49:13 +0200

    mejoras en pep8

--- pep8.sh
+++ pep8.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+find . -type f -name "*.py" -print0 | xargs -0 sed -i '1 s/^#!\/usr\/bin\/python/#!\/usr\/bin\/env python/'
 autoflake --in-place --recursive .
 autopep8 --in-place --recursive .
 isort -rc .
 
-if [ -f ./requirements.txt ]; then
-    pipreqs --print . | sort > requirements.txt
+if [ -f ./requirements.txt ] || [ "$1" == "-r" ]; then
+  pipreqs . --force
+  if grep -q "beautifulsoup4" requirements.txt; then
+  if grep -q -e "[\"']lxml[\"']" --include="*.py" -r .; then
+    pip3 freeze | grep lxml >> requirements.txt
+  fi
+  fi
+  sort -o requirements.txt requirements.txt
 fi
