386c871768be1dc16dbe73b13f20dd8ec0b2af0a
s-nt-s <santos82h@gmail.com>
2020-02-20 20:37:29 +0100

    usar dig para encontrar ip

--- getip.sh
+++ getip.sh
@@ -1,11 +1,36 @@
-#!/bin/sh
-IP=$(curl -s icanhazip.com)
-if [ -z "$IP" ]; then
-	IP=$(curl -s ifconfig.me)
-fi
-if [ -z "$IP" ]; then
-        exit 1
-fi
-echo "$IP"
-exit 0
+#!/bin/bash
+DBG="$1"
 
+MY_DIG=""
+
+if command -v dig >/dev/null 2>&1; then
+  MY_DIG="dig"
+fi
+if command -v kdig >/dev/null 2>&1; then
+  MY_DIG="kdig"
+fi
+
+if [ ! -z "$MY_DIG" ]; then
+  if [ "$DBG" == "-v" ]; then
+    "$MY_DIG" +short myip.opendns.com @resolver1.opendns.com
+  else
+    "$MY_DIG" +short myip.opendns.com @resolver1.opendns.com 2>/dev/null
+  fi
+  exit $?
+fi
+
+getIp () {
+   IP=$(wget -q -T 4 "http://$1" -O -)
+   if [ ! -z "$IP" ] && [ ${#IP} -ge 8 ] && [ ${#IP} -le 16 ]; then
+      echo "$IP"
+      exit 0
+   fi
+   if [ "$DBG" == "-v" ]; then
+      wget "http://$1" -O -
+   fi
+}
+getIp ifconfig.me
+getIp icanhazip.com
+getIp ipecho.net/plain
+getIp ifconfig.co
+exit 1
