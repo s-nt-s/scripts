88d947736415e90208f87edd9997e2253aa5c264
s-nt-s <santos82h@gmail.com>
2020-03-23 12:58:00 +0100

    vpn parametros

--- vpn.sh
+++ vpn.sh
@@ -1,12 +1,54 @@
 #!/bin/bash
 
-VPN=$(nmcli con | grep " vpn " | cut -d' ' -f1)
-if [ -z "$VPN" ]; then
-    echo "VPN no encontrada. nmcli con:"
-    nmcli con
-    exit 1
+P_ON=0
+P_OFF=0
+VPN=""
+
+while [[ $# -gt 0 ]]; do
+key="$1"
+case $key in
+    "on")
+        P_ON=1
+        shift
+    ;;
+    "off")
+        P_OFF=1
+        shift
+    ;;
+    *)
+    if [ -z "$VPN" ]; then
+        VPN="$1"
+    else
+        VPN="$VPN $1"
+    fi
+    shift
+    ;;
+esac
+done
+
+if [ ! -z "$VPN" ]; then
+    if ! nmcli con | grep " vpn " | cut -d' ' -f1 | grep -q "$VPN"; then
+        echo "VPN $VPN no encontrada"
+        echo "$ nmcli con:"
+        nmcli con
+        exit 1
+    fi
+else
+    VPN=$(nmcli con | grep " vpn " | cut -d' ' -f1)
+    if [ -z "$VPN" ]; then
+        echo "VPN no encontrada"
+        echo "$ nmcli con:"
+        nmcli con
+        exit 1
+    fi
 fi
-ST=$(nmcli con | grep " vpn " | sed 's/  */ /g' | cut -d' ' -f4)
+ST=$(nmcli con | grep " vpn " | sed 's/  */ /g' | grep -E "^${VPN}" | cut -d' ' -f4)
+if [ $P_ON -eq 1 ] && [ "$ST" != "--" ]; then
+    exit 0
+fi
+if [ $P_OFF -eq 1 ] && [ "$ST" == "--" ]; then
+    exit 0
+fi
 if [ "$ST" == "--" ]; then
     echo "$VPN off -> on"
     nmcli con up id "$VPN"
