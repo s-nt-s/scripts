de8edc53bed6b7f5ffb00b6c46f49d1d6c42be57
s-nt-s <santos82h@gmail.com>
2022-02-28 07:36:11 +0100

    checkntlmproxy.sh

--- checkntlmproxy.sh
+++ checkntlmproxy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+PRM1="$1"
+
 function ask {
   local myvar=""
   local question=""
@@ -9,7 +11,9 @@ function ask {
   else
     question="# $2: [$3]"
   fi
-  read "$1" "$question" myvar
+  if [ "$PRM1" == "--ask" ] || [ -z "$3" ]; then
+    read "$1" "$question" myvar
+  fi
   if [ -z "$myvar" ]; then
      myvar="$3"
   fi
@@ -26,6 +30,6 @@ if [ -z "$USERPASS" ]; then
 fi
 
 URL="http://detectportal.firefox.com/success.txt"
-echo "$ curl --proxy-user $USERNAME:******** --proxy $PROXYADD $URL"
+echo "$ curl --proxy-ntlm --proxy-user $USERNAME:* --proxy $PROXYADD $URL"
 
 curl --proxy-ntlm --proxy-user "$USERNAME:$USERPASS" --proxy "$PROXYADD" "$URL"
