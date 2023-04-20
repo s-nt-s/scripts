9f563dfe1895dad4c0470360a29e38994c56a301
s-nt-s <santos82@gmail.com>
2023-04-20 21:53:57 +0200

    excluir más casos cuando es una raspberry

--- linux-backup.sh
+++ linux-backup.sh
@@ -48,13 +48,13 @@ cat > "${OUT}exclude.txt" <<EOL
 /CheckPoint
 EOL
 
-printf "%s\0" "${HOMES[@]}" | xargs -0 -I{} find '{}' -maxdepth 1 -name '.*' -printf '/%P\n' | sort | uniq | grep -v -E '^\/\.(config|icons|local|mozilla|pingus|purple|pynagram|texmf-var|TeXworks|thunderbird|aws|aws-sam|cdk|cert|claws-mail|davmail.properties|ecryptfs|eteks|face|filezilla|gconf|gdfuse|gnupg|hedgewars|kube|netrc|k8slens|pgpass|Private|proxychains|RapidSVN|pypirc|ssh|subversion|vscode|xmpp.yml|docker|bash_aliases|profile.*|pgadmin.*|git.*|elect.*|dbeaver.*|bit.*|mysql.*|shar.*-ri.*b)$' >> "${OUT}exclude.txt"
+printf "%s\0" "${HOMES[@]}" | xargs -0 -I{} find '{}' -maxdepth 1 -name '.*' -printf '/%P\n' | sort | uniq | grep -v -E '^\/\.(toxmpp|config|icons|local|mozilla|pingus|purple|pynagram|texmf-var|TeXworks|thunderbird|aws|aws-sam|cdk|cert|claws-mail|davmail.properties|ecryptfs|eteks|face|filezilla|gconf|gdfuse|gnupg|hedgewars|kube|netrc|k8slens|pgpass|Private|proxychains|RapidSVN|pypirc|ssh|subversion|vscode|xmpp.yml|docker|bash_aliases|profile.*|pgadmin.*|git.*|elect.*|dbeaver.*|bit.*|mysql.*|shar.*-ri.*b)$' >> "${OUT}exclude.txt"
 
 printf "%s\0" "${HOMES[@]}" | xargs -0 -I{} find '{}' -maxdepth 2 -path '{}.local/*' -printf '/%P\n' | sort | uniq | grep -v -E '^\/\.local/(share)$' >> "${OUT}exclude.txt"
 
 printf "%s\0" "${HOMES[@]}" | xargs -0 -I{} find '{}' -maxdepth 3 -path '{}.local/share/*' -printf '/%P\n' | sort | uniq | grep -v -E '^\/\.local/share/(greg|calibre.*|DBeaver.*|fonts|gajim|gvfs-metadata|keyrings|notes|remmina|Tele.*Desktop|tomboy)$' >> "${OUT}exclude.txt"
 
-printf "%s\0" "${HOMES[@]}" | xargs -0 -I{} find '{}' -maxdepth 2 -path '{}.config/*' -printf '/%P\n' | sort | uniq | grep -v -E '^\/\.config/(greg|calibre.*|chromium|filezilla|freerdp|gajim|google-chrome|hexchat|kdenlive.*|keepassx|Lens|Microsoft.*|mon.*-project|Mumble|remmina|skypeforlinux|teams|tomboy|transmission.*|VirtualBox)$' >> "${OUT}exclude.txt"
+printf "%s\0" "${HOMES[@]}" | xargs -0 -I{} find '{}' -maxdepth 2 -path '{}.config/*' -printf '/%P\n' | sort | uniq | grep -v -E '^\/\.config/(geany|greg|calibre.*|chromium|filezilla|freerdp|gajim|google-chrome|hexchat|kdenlive.*|keepassx|Lens|Microsoft.*|mon.*-project|Mumble|remmina|skypeforlinux|teams|tomboy|transmission.*|VirtualBox)$' >> "${OUT}exclude.txt"
 
 printf "%s\0" "${HOMES[@]}" | xargs -0 -I{} find '{}' -maxdepth 2 -path '{}.mozilla/*' -printf '/%P\n' | sort | uniq | grep -v -E '^\/\.mozilla/(firefox)$' >> "${OUT}exclude.txt"
 
@@ -63,6 +63,26 @@ printf "%s\0" "${HOMES[@]}" | xargs -0 -I{} find '{}' -maxdepth 4 -path '{}wks/*
 #printf "%s\0" "${HOMES[@]}" | xargs -0 -I{} find '{}' -maxdepth 4 -path '{}wks/*' -type d -name "node_modules" -printf '/%P\n' | sort | uniq
 
 find /etc/passwd /etc/shadow /etc/cron* /etc/fstab /etc/host* /etc/systemd/ /etc/nginx /etc/apache2/ /etc/aliases /etc/environment /etc/sudo* -type f ! -name .placeholder ! -empty | sed 's|^/etc||' > "${OUT}etc.txt"
+
+if [ "$(cat /etc/issue | cut -d' ' -f1 | tr '[:upper:]' '[:lower:]')" == "raspbian" ]; then
+cat >> "${OUT}exclude.txt" <<EOL
+/.claws-mail
+/.eteks
+/.gconf
+/.hedgewars
+/.icons
+/.pingus
+/.purple
+/.pynagram
+/.profile
+/.local/share/gajim
+/.local/share/gvfs-metadata
+/.config/geany
+/.config/chromium
+/.config/gajim
+/.config/transmission-remote-gtk
+EOL
+fi
 
 DHM="/etc/"
 HOUT="${OUT}$(basename $DHM)"
