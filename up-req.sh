153efa8c892035aab4c0ce88c7e6e1c8ded36a0d
s-nt-s <santos82h@gmail.com>
2024-08-08 19:06:13 +0200

    sincroniza el requirements.txt con lo que realmente esta instalado

---
+++ up-req.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+RQ="$1"
+if [ -z "$RQ" ]; then
+    RQ="./requirements.txt"
+fi
+
+if [ ! -f "$RQ" ]; then
+    echo "File not found: $RQ"
+    exit 1
+fi
+
+NOTFOUND=()
+
+while read -r lib; do
+    VS=$(pip show "$lib" 2>/dev/null | grep -E "^Version: " | awk '{print $2}')
+    if [ -n "$VS" ]; then
+        echo "$lib==$VS"
+    else
+        NOTFOUND+=("$lib")
+    fi
+done < <(sed 's/[=~<>].*//g' "$RQ" | sort | uniq)
+
+if [ ${#NOTFOUND[@]} -gt 0 ]; then
+    echo "# The following packages were not found:"
+    for lib in "${NOTFOUND[@]}"; do
+        escaped_lib=$(printf '%s\n' "$lib" | sed 's/[][\.*^$(){}?+|/]/\\&/g')
+        grep -E "^$escaped_lib([=~<>].*)?$" "$RQ"
+    done
+fi
