c48be263b166a0e36268ce0b082650ac2ae35919
s-nt-s <santos82h@gmail.com>
2026-04-13 19:31:07 +0000

    lanzador de scripts de python

--- pyrun.sh
+++ pyrun.sh
@@ -5,11 +5,11 @@ SCRIPT="$(realpath "$1")"
 shift
 
 DIR="$(dirname "$SCRIPT")"
-
+PARENT="$(dirname "$DIR")"
 if [ -x "$DIR/.venv/bin/python" ]; then
     PYTHON="$DIR/.venv/bin/python"
-elif [ -x "$DIR/venv/bin/python" ]; then
-    PYTHON="$DIR/venv/bin/python"
+elif [ -x "$PARENT/.venv/bin/python" ]; then
+    PYTHON="$PARENT/.venv/bin/python"
 else
     echo "python venv not found" >&2
     exit 1
